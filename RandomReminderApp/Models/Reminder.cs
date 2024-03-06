53c04361fc36804da980ca3b80d16e07b5606997
micihime <hroncovam@yahoo.com>
2024-03-06 14:15:12 +0100

    Adding Reminder class with some basic properties

---
+++ RandomReminderApp/Models/Reminder.cs
@@ -0,0 +1,11 @@
+﻿namespace RandomReminderApp.Models
+{
+    public class Reminder
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } 
+        public TimeOnly From { get; set; }
+        public TimeOnly To { get; set; }
+    }
+}
